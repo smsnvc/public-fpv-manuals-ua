44817bb3247de9ea16874aee30dace6cc793c791
smsnvc <157111335+smsnvc@users.noreply.github.com>
2025-06-14 17:43:00 +0300

    Deploy website - based on 0ebfeb66a4d689f700bf8be599b5cd16a4b81edb

---
+++ assets/js/acecf23e.d799d962.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/public-fpv-manuals-ua/blog","blogTitle":"Blog","authorsListPath":"/public-fpv-manuals-ua/blog/authors"}')}}]);
